8a0b100fb2aa7c7eff1e4c63bd410cbf4f759987
Louise <t7861231@gmail.com>
2018-10-05 12:30:03 +0800

    add read file

--- products.py
+++ products.py
@@ -2,10 +2,13 @@
 products = []
 with open('products.csv', 'r',encoding='utf-8') as f:
 	for line in f:
+		if '商品,價格' in line:
+			continue #繼續 迴圈繼續 跳到下一回
 		name, price = line.strip().split(',')  #split() 完變清單 ,等號前直接定義清單名稱
 		products.append([name, price])
 print(products)
 
+#讓使用者輸入
 while True:
 	name = input('請輸入商品名稱:')
 	if name == 'q': #quit
@@ -18,11 +21,12 @@ while True:
 	products.append([name, price])
 print(products)
 
+#印出所有購買紀錄
 #products[0][0] #存取二維清單
-
 for p in products: #for loop 搞清楚清單內每一項目
 	print(p[0], '的價格是', p[1])
 
+#寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:  #'w' 寫入模式(write)
 	f.write('商品,價格\n')
 	for p in products:
